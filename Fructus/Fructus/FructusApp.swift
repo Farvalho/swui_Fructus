ed6c20a162c26ea06a93c82b46464a5907870dee
Fabio Carvalho <farvalho@outlook.com>
2022-08-29 15:27:27 +0100

    +Add: Default project & assets

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Fábio Carvalho on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
